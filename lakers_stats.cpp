3153cbece498116b5b57abfb4368316031902cd8
pedrogallardoaceves <pedrogallardoaceves@gmail.com>
2022-10-27 22:55:57 -0500

    Finished both menus

--- lakers_stats.cpp
+++ lakers_stats.cpp
@@ -239,17 +239,55 @@ void ShowTop5(vector <Player> p)
 
 
 
+Player AddNew(string s)
+{
+    int i[6];
+    float f[6];
+    cout<<"How many Points has "<<s<<" made?\n";
+    cin>>i[0];
+    cout<<"What is his point average?\n";
+    cin>>f[0];
+    cout<<"How many Three pointers has "<<s<<" made?\n";
+    cin>>i[1];
+    cout<<"What is his three points average?\n";
+    cin>>f[1];
+    cout<<"How many Freethrows has "<<s<<" made?\n";
+    cin>>i[2];
+    cout<<"What is his freethrow average?\n";
+    cin>>f[2];
+    cout<<"How many Blocks has "<<s<<" made?\n";
+    cin>>i[3];
+    cout<<"What is his block average?\n";
+    cin>>f[3];
+    cout<<"How many Steals has "<<s<<" made?\n";
+    cin>>i[4];
+    cout<<"What is his steal average?\n";
+    cin>>f[4];
+    cout<<"How many Assists has "<<s<<" made?\n";
+    cin>>i[5];
+    cout<<"What is his assist average?\n";
+    cin>>f[5];
+    Player p(s,i[0],f[0],i[1],f[1],i[2],f[2],i[3],f[3],i[4],f[4],i[5],f[5]);
+    return p;
+}
+
+Player ModifyList(string c)
+{
+    return AddNew(c);
+}
+
 
 int main()
 {
     string CorA, x;
+    char cad;
     //Name, points, threes, FT, blocks, steals, assists
     Player Anthony ("Anthony Davis", 74, 24.7, 2, 0.66, 16, 5.3, 7, 2.3, 8, 2.7, 3, 1);
     Player Austin ("Austin Reaves", 14, 4.7, 2, 0.7, 4, 1.3, 0, 0, 1, .3, 4, 1.3);
     Player Damian("Damian Jones", 9, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1);
     Player Juan("Juan Toscano", 12, 4, 1, 0.3, 1, 0.3, 0, 0, 2, 0.7, 3, 1);
     Player Kendrick("Kendrick Nunn", 13, 4.3, 3, 1, 0, 0, 1, 0.3, 1, 0.3, 5, 1.7);
-    Player Lebron ("Lebron James", 82, 27.3, 7, 0.259, 8, 5.7, 4, 1.33, 3, 1, 22, 7.3);
+    Player Lebron ("Lebron James", 82, 27.3, 7, 2.3, 8, 5.7, 4, 1.33, 3, 1, 22, 7.3);
     Player Lonnie("Lonnie Walker", 46, 15.3, 3, 1, 7, 2.3, 2, 0.7, 4, 1.3, 9, 3);
     Player Max("Max Christie", 9, 3, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0);
     Player Patrick("Patrick Beverley", 15, 5, 3, 1, 6, 2, 4, 1.3, 7, 2.3, 8, 2.7);
@@ -263,89 +301,99 @@ int main()
     players.push_back(Lonnie);players.push_back(Max);players.push_back(Patrick);
     players.push_back(Troy);players.push_back(Russel);players.push_back(Wenyen);
 
-
-    cout<<"Type a 'c' if your are a client or the password if you are the administrator\n";//the password is potato
-    cin>>CorA;
-    if(CorA=="c" || CorA=="C")
-    {
-        int op;
-        do
+    do{
+        cout<<"Type a 'c' if your are a client or the password if you are the administrator\n";//the password is potato
+        cin>>CorA;
+        if(CorA=="c" || CorA=="C")
         {
-            cout<<"\n-------------USERS MENU--------------\n";
-            cout<<"Select one option only: \n";
-            cout<<"1 --\tShow all the team mebers in a file\n";
-            cout<<"2 --\tShow all the team mebers and their stats in a file\n";
-            cout<<"3 --\tShow all the stats of a specific player\n";
-            cout<<"4 --\tShow specific stats from a player or players \n";
-            cout<<"5 --\tShow the player with the most points in the season\n";
-            cout<<"6--\tShow the top 5 players in the team\n";
-            cout<<"7--\tEXIT\n\n";
-            cin>>op;
-            switch(op)
+            int op;
+            do
             {
-                case 1:
-                    ShowNames(players);
-                    break;
-                case 2:
-                    ShowAll(players);
-                    break;
-                case 3:
-                    cout<<"Write the full name of the player of which you want to know the stats\t example 'Lebron James'\n";
-                    cin.ignore();
-                    getline(cin,x);
-                    ShowPlayer(players, x);
-                    break;
-                case 4:
-                    ShowSpecific(players);
-                    break;
-                case 5:
-                    ShowMostPoints(players);
-                    break;
-                case 6:
-                    ShowTop5(players);
-                    break;
-                case 7:
-                    cout<<"\nThank you, come back soon!!\n\n";
-                    break;
-                default:
-                    cout<<"Invalid option, try again\n\n";
-                    break;
-            }
-        }while(op!=7);
-    }
-    else if(CorA=="potato")
-    {
-        int op;
-        do{
-            cout<<"\n-------------ADMIN MENU--------------\n";
-            cout<<"Select one option only: \n";
-            cout<<"1 --\tAdd a new team player\n";
-            cout<<"2 --\tModify a player's information\n";
-            cout<<"3 --\tEXIT\n";
-            cin>>op;
-            switch(op)
-            {
-                case 1:
-                    //AddNew();
-                    break;
-                case 2:
-                    //ModifyList();
-                    break;
-                case 3:
-                    cout<<"\nThank you, come back soon!!\n\n";
-                    break;
-                default:
-                    cout<<"Invalid option, try again\n\n";
-                    break;
-            }
-        }while(op!=3);
+                cout<<"\n-------------USERS MENU--------------\n";
+                cout<<"Select one option only: \n";
+                cout<<"1 --\tShow all the team mebers in a file\n";
+                cout<<"2 --\tShow all the team mebers and their stats in a file\n";
+                cout<<"3 --\tShow all the stats of a specific player\n";
+                cout<<"4 --\tShow specific stats from a player or players \n";
+                cout<<"5 --\tShow the player with the most points in the season\n";
+                cout<<"6--\tShow the top 5 players in the team\n";
+                cout<<"7--\tEXIT\n\n";
+                cin>>op;
+                switch(op)
+                {
+                    case 1:
+                        ShowNames(players);
+                        break;
+                    case 2:
+                        ShowAll(players);
+                        break;
+                    case 3:
+                        cout<<"Write the full name of the player of which you want to know the stats\t example 'Lebron James'\n";
+                        cin.ignore();
+                        getline(cin,x);
+                        ShowPlayer(players, x);
+                        break;
+                    case 4:
+                        ShowSpecific(players);
+                        break;
+                    case 5:
+                        ShowMostPoints(players);
+                        break;
+                    case 6:
+                        ShowTop5(players);
+                        break;
+                    case 7:
+                        cout<<"\nThank you, come back soon!!\n\n";
+                        break;
+                    default:
+                        cout<<"Invalid option, try again\n\n";
+                        break;
+                }
+            }while(op!=7);
+        }
+        else if(CorA=="potato")
+        {
+            string y;
+            int op;
+            do{
+                cout<<"\n-------------ADMIN MENU--------------\n";
+                cout<<"Select one option only: \n";
+                cout<<"1 --\tAdd a new team player\n";
+                cout<<"2 --\tModify a player's information\n";
+                cout<<"3 --\tEXIT\n";
+                cin>>op;
+                switch(op)
+                {
+                    case 1:
+                        cout<<"What is the name of the new player?\n";
+                        cin.ignore();
+                        getline(cin,y);
+                        players.push_back(AddNew(y));
+                        break;
+                    case 2:
+                        cout<<"Write the name of the player thet you want to modify\n";
+                        cin.ignore();
+                        getline(cin,y);
+                        for(int i=0; i<players.size(); i++) if(y==players.at(i).GetName())players.erase(players.begin()+i-1);
+                        players.push_back(ModifyList(y));
+                        break;
+                    case 3:
+                        cout<<"\nThank you, come back soon!!\n\n";
+                        break;
+                    default:
+                        cout<<"Invalid option, try again\n\n";
+                        break;
+                }
+            }while(op!=3);
 
-    }
-    else
-    {
-        cout<<"Invalid input, try again\n\n";
-        main();
-    }
-
+        }
+        else
+        {
+            cout<<"Invalid input, try again\n\n";
+            main();
+        }
+        cout<<"Type a 'y' if you want to keep browsing, if not, type 'n'\n";
+        cin>>cad;
+    }while(cad!='n' || cad!='N');
     return 0;
 }
